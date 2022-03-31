b32ddcb52f26141edec9d1a8b9204fc4ca78e41e
Achaursiya <99475947+Achaursiya@users.noreply.github.com>
2022-03-31 17:03:17 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 DC_VERSION="6.0.4"
-#DC_DIRECTORY=$HOME/OWASP-Dependency-Check
-#DC_PROJECT="dependency-check scan: $(pwd)"
+DC_DIRECTORY=$HOME/OWASP-Dependency-Check
+DC_PROJECT="dependency-check scan: $(pwd)"
 DATA_DIRECTORY="$DC_DIRECTORY/data"
 CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
 
